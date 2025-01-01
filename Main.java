3ac0c1ff9943378852a314e00004c15a2eb3d265
RMJ15 <rmur19814@gmail.com>
2025-01-01 17:42:25 +0500

    Исправление задачи #1

--- Main.java
+++ Main.java
@@ -1,8 +1,38 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        int x = 6;
-        System.out.println(x);
+        //Считывает с консоли имя текстового файла.
+        Scanner input = new Scanner(System.in);
+        String filePath = input.nextLine();
+
+
+        try{
+            Path path = Paths.get(filePath);
+            List<String> text = Files.readAllLines(path);
+
+
+            for(String contentFile: text){
+                for(Character symbolText: contentFile.toCharArray()){
+                    if(symbolText == '.' || symbolText == ',' || symbolText == ' ') {
+                        continue;
+                    }
+                    System.out.print(symbolText + "");
+                }
+            }
+            
+
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
